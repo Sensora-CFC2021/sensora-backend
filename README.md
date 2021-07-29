f0476e3292cc10ddb2596c201c9c1eacba071dd8
macbookpro <ulziiburend@gmail.com>
2021-07-29 18:14:33 +0800

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# sensora-backend
+# Sensora-backend
+
+## Call for code challenge 2021
+
+
+This repository contains the source code for API backend which is using Django REST framework work with sql database. 
+
+Required versions 
+```
+Python 3.7 
+pip 21.2
+```
+
+### STEP 1. Install requirements
+``` commandline
+pip install -r requirements.txt
+```
+### STEP 2. Migration
+
+``` commandline
+python manage.py migrate
+```
+
+### STEP 3. Run application
+
+``` commandline
+python manage.py runserver
+```
+
+Locate [localhost:8000](http://localhost:8000)
+
--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kmyu#+qw9@#@*(3wg8$wn!$6n$hn2wtf2#90)9r9_14d%k$dp('
+SECRET_KEY = os.environ.get('SECRET_KEY', 'foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=1))
 
 ALLOWED_HOSTS = []
 
